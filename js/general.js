0bb70581943194738012815713aeef3644ee87d9
robert freeman <free0056@algonquinlive.com>
2014-01-20 16:09:15 -0500

    add carousel

--- js/general.js
+++ js/general.js
@@ -112,7 +112,7 @@ var trex = {
 
 console.log( trex.name );
 
-*/
+
 
 // $('.ball').fadeout();
 
@@ -151,6 +151,43 @@ $('#btn-down').on('click', function () {
 });
 
 
+*/
+
+var $items = $('.items img')
+
+var switchItem = function (current, incoming) {
+	$items.eq(incoming).attr('data-state', 'incoming').fadeIn(250, function () {
+		$items.eq(current).hide().attr('data-state', '')
+		$items.eq(incoming).attr('data-state', 'current')	
+	});
+};
+
+
+$items.filter(':not([data-state="current"])').hide();
+
+$('.next').on('click', function() {
+	var current = $items.filter('[data-state="current"]').index();
+	var next = current +1;
+
+	if (next > $items.length - 1) {
+		next = 0;
+	}
+
+	switchItem(current, next);
+})
+
+
+
+$items.filter(':not([data-state="current"])').hide();
+
+$('.previous').on('click', function() {
+	var current = $items.filter('[data-state="current"]').index();
+	var previous = current -1;
+
+
+
+	switchItem(current, previous);
+})
 
 
 
